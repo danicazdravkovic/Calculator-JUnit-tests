64ffb649f6643af8dbc89368f6e4beb4fbfefefb
danica zdravkovic <zdravkovic870@gmail.com>
2023-05-09 16:31:26 +0200

    Project without tests

---
+++ src/main/java/com/automatizacija/calculator/Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.automatizacija.calculator;
+
+/**
+ *
+ * @author nodas
+ */
+public class Calculator {
+
+  public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws Exception {
+        if (b == 0) {
+            throw new Exception("Can't divide by zero");
+        }
+        return a / b;
+    }
+}
